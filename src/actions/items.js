1fa4e51ec969e4cffb506d74770a6072001af8c3
Viktor Jansson <viktorjansson@Viktors-MacBook-Air.local>
2016-12-06 23:23:01 +0100

    refactored to es6 syntax

--- src/actions/items.js
+++ src/actions/items.js
@@ -4,57 +4,59 @@ let C = require('../constants'),
     firebaseRef = new Firebase(C.FIREBASE_URL),
     itemsRef = firebaseRef.child("items");
 
-module.exports = {
-    submitNewItem: function(item){
-        return function(dispatch, getState){
+
+export function submitNewItem(item){
+    return function(dispatch, getState){
+        let newItem = {
+            item: item,
+            checked: false
+        }
+        itemsRef.push(newItem);
+    }
+}
+
+export function checkItem(item){
+    return function(dispatch, getState){
+        if(item.checked){
             let newItem = {
-                item: item,
+                item: item.item,
                 checked: false
             }
-            itemsRef.push(newItem);
-        }
-    },
-    checkItem: function(item){
-        return function(dispatch, getState){
-            if(item.checked){
-                let newItem = {
-                    item: item.item,
-                    checked: false
-                }
-                itemsRef.child(item.key).update(newItem);
-            } else {
-                let newItem = {
-                    item: item.item,
-                    checked: true
-                }
-                itemsRef.child(item.key).update(newItem);
+            itemsRef.child(item.key).update(newItem);
+        } else {
+            let newItem = {
+                item: item.item,
+                checked: true
             }
-            
+            itemsRef.child(item.key).update(newItem);
         }
-    },
-    submitDeleteItem: function(key){
-        return function(dispatch, getState){
-            itemsRef.child(key).remove();
-        }
-    },
-    listeningToItems: function(){
-        return function(dispatch, getState){
-            itemsRef.on("value", function(snapshot){
-                let itemsArray = [];
-                snapshot.forEach(function(item){
-                    let itemData = item.val();
-                    let key = item.key();
-                        itemsArray.push({
-                            item: itemData.item,
-                            checked: itemData.checked,
-                            key: key
-                        });
-                });
-                dispatch({
-                    type: C.RECEIVE_ITEM,
-                    data: itemsArray
-                });
-            });
-        }
+
     }
-};
+}
+
+export function submitDeleteItem(key){
+    return function(dispatch, getState){
+        itemsRef.child(key).remove();
+    }
+}
+
+export function listeningToItems(){
+    return function(dispatch, getState){
+        itemsRef.on("value", function(snapshot){
+            let itemsArray = [];
+            snapshot.forEach(function(item){
+                let itemData = item.val();
+                let key = item.key();
+                    itemsArray.push({
+                        item: itemData.item,
+                        checked: itemData.checked,
+                        key: key
+                    });
+            });
+            dispatch({
+                type: C.RECEIVE_ITEM,
+                data: itemsArray
+            });
+        });
+    }
+}
